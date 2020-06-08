2a5dc1779c764aef0840542e3d5bc44f498ca2f9
henry <dru@Henrys-MacBook-Pro.local>
2020-06-08 16:49:16 +0300

    Create and list projects

--- start/routes.js
+++ start/routes.js
@@ -1,18 +1,5 @@
 "use strict";
 
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| Http routes are entry points to your web application. You can create
-| routes for different URLs and bind Controller actions to them.
-|
-| A complete guide on routing is available here.
-| http://adonisjs.com/docs/4.1/routing
-|
-*/
-
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use("Route");
 Route.group(() => {
@@ -21,4 +8,8 @@ Route.group(() => {
 
   // register
   Route.post("auth/login", "UserController.login");
+
+  // projects
+  Route.get("projects", "ProjectController.index").middleware("auth");
+  Route.post("projects", "ProjectController.create").middleware("auth");
 }).prefix("api");
